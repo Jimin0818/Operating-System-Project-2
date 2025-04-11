b70b101d6df2d92c9102b5a4e1e1f1eff5ead0b6
Ji Min Yoon <151479951+Jimin0818@users.noreply.github.com>
2025-04-10 23:23:24 -0500

    Update bank.py

--- bank.py
+++ bank.py
@@ -24,3 +24,29 @@ class Teller(threading.Thread):
         self.customerAvailability = threading.Semaphore(0) # availability once taking a customer
         self.customer = None
         self.transaction = None
+
+  def run(self):
+        global customer_served
+        if self.transaction == "Withdraw":
+                      print(f"Teller {self.id} [Teller {self.id}]: Go to Manager")
+                      manager.acquire()
+                      print(f"Teller {self.id} [Teller {self.id}]: Interact with Manager")
+                      time.sleep(random.uniform(0.005, 0.03))
+                      print(f"Teller {self.id} [Teller {self.id}]: Manager complete")
+                      manager.release()
+                print(f"Teller {self.id} [Teller {self.id}]: Going to Safe")
+                bank_safe.acquire()
+                print(f"Teller {self.id} [Teller {self.id}]: In the Safe")
+                time.sleep(random.uniform(0.01, 0.05))
+                print(f"Teller {self.id} [Teller {self.id}]: Done in safe")
+                bank_safe.release()
+
+                print(f"Teller {self.id} [Customer {self.customer.id}]: Transaction Complete")
+                self.customer.complete.release()
+
+                self.customer.leave.acquire()
+
+                with customerServed_lock:
+                      numCustomerServed += 1
+                      if numCustomerServed >= numCustomerServed:
+                            break
