f6b80ef95b025879217a7aeb534a39e024122e13
Ji Min Yoon <151479951+Jimin0818@users.noreply.github.com>
2025-04-10 21:41:05 -0500

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,1 +1,18 @@
+import threading
+import time
+from queue import Queue
+import random
 
+tellers = 3
+customers = 50
+
+bank_door = threading.Semaphore(2) 
+bank_safe = threading.Semaphore(2) 
+manager = threading.Semaphore(1) 
+teller_ready = threading.Semaphore(0) 
+
+teller_queue = Queue()
+customer_queue = Queue()
+
+numCustomerServed = 0
+numCustomerServed_lock = threading.lock()
